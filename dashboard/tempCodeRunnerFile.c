3975bf08ae16a6695ab9d45188b74afc7e18991a
dharmitaliya <dharm.italiya5151@gmail.com>
2024-10-17 23:48:34 +0530

    Initial commit

---
+++ dashboard/tempCodeRunnerFile.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a)
+if(a>0)
+{
+printf("number is positive");
+}
+else if(a<0)
+{
+printf("number is negative");
+}
+else
+{
+printf("number ids zero");
+}
+return 0;
+}
